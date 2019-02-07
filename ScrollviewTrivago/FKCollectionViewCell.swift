6cfbe44c7b6927f59f0dc28d27888e2efa97cf08
wflogin <wflogin@Daniels-iMac21.local>
2019-02-07 17:46:46 +0530

    Scrollview

---
+++ ScrollviewTrivago/FKCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FKCollectionViewCell.swift
+//  ScrollviewTrivago
+//
+//  Created by wflogin on 07/02/19.
+//  Copyright © 2019 wflogin. All rights reserved.
+//
+
+import UIKit
+
+class FKCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ ScrollviewTrivago/helper.swift
@@ -0,0 +1,9 @@
+//
+//  helper.swift
+//  ScrollviewTrivago
+//
+//  Created by wflogin on 07/02/19.
+//  Copyright © 2019 wflogin. All rights reserved.
+//
+
+import Foundation
